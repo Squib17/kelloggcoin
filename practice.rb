02260500ce44fd7a87456aa49b8ffaf429d77537
Saquib Hasnain <saquib.hasnain@kellogg.northwestern.edu>
2024-01-30 06:38:09 +0000

    Crypto_Kellogg

--- practice.rb
+++ practice.rb
@@ -27,3 +27,60 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+#Method 1: Tried this myself
+
+ben = 0
+brian = 0
+evan = 0
+anthony = 0
+for transfer in blockchain
+  if transfer["from_user"] == "ben"
+    ben = ben - transfer["amount"]
+  elsif transfer["from_user"] == "brian"
+    brian = brian - transfer["amount"]
+  elsif transfer["from_user"] == "evan"
+    evan = evan - transfer["amount"]
+  elsif transfer["from_user"] == "anthony"
+    anthony = anthony - transfer["amount"]
+  end
+
+  if transfer["to_user"] == "ben"
+    ben = ben + transfer["amount"]
+  elsif transfer["to_user"] == "brian"
+    brian = brian + transfer["amount"]
+  elsif transfer["to_user"] == "evan"
+    evan = evan + transfer["amount"]
+  elsif transfer["to_user"] == "anthony"
+    anthony = anthony + transfer["amount"]
+  end
+end
+
+puts "Ben's KelloggCoin balance is #{ben}"
+puts "Brian's KelloggCoin balance is #{brian}"
+puts "Evan's KelloggCoin balance is #{evan}"
+puts "Anthony's KelloggCoin balance is #{anthony}"
+
+
+
+# Method 2: Brainstorm a bit with GPT
+
+# Initialize an empty hash to store balances
+balances = Hash.new(0)
+# Process transfers using a for loop
+for x in 0...blockchain.length
+  transfer = blockchain[x]             #get detail of each transfer 
+  if transfer["from_user"]             #this will be always true
+  balances[transfer["from_user"]]= balances[transfer["from_user"]] - transfer["amount"]
+  end
+  balances[transfer["to_user"]] = balances[transfer["to_user"]] + transfer["amount"]
+end
+
+# Output results using  for loop
+users = balances.keys    #get the names of all users
+for i in 0...users.length
+  user = users[i]
+  balance = balances[user]
+  user_name = user&.capitalize || "System"               #Capitalizing the name  
+  puts "Method 2: #{user_name}'s KelloggCoin balance is #{balance}"
+end
+
